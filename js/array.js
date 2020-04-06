d6cdd17421d2a5294f5fe8cbd7e48d7749d5ebbc
chenlei <1183470830@qq.com>
2020-04-06 22:30:54 +0800

    Create array.js

---
+++ js/array.js
@@ -0,0 +1,8 @@
+import letter from './letter.js';
+const {
+  letter_A, letter_B, letter_C, letter_D, letter_E, letter_F, letter_G, letter_H, letter_I, letter_J, letter_K, letter_L, letter_M, letter_N, letter_O, letter_P, letter_Q, letter_R, letter_S, letter_T, letter_U, letter_V, letter_W, letter_X, letter_Y, letter_Z
+} = letter;
+const arr_A = letter_A.split('\n'), arr_B = letter_B.split('\n'), arr_C = letter_C.split('\n'), arr_D = letter_D.split('\n'), arr_E = letter_E.split('\n'), arr_F = letter_F.split('\n'), arr_G = letter_G.split('\n'), arr_H = letter_H.split('\n'), arr_I = letter_I.split('\n'), arr_J = letter_J.split('\n'), arr_K = letter_K.split('\n'), arr_L = letter_L.split('\n'), arr_M = letter_M.split('\n'), arr_N = letter_N.split('\n'), arr_O = letter_O.split('\n'), arr_P = letter_P.split('\n'), arr_Q = letter_Q.split('\n'), arr_R = letter_R.split('\n'), arr_S = letter_S.split('\n'), arr_T = letter_T.split('\n'), arr_U = letter_U.split('\n'), arr_V = letter_V.split('\n'), arr_W = letter_W.split('\n'), arr_X = letter_X.split('\n'), arr_Y = letter_Y.split('\n'), arr_Z = letter_Z.split('\n');
+export default {
+  arr_A, arr_B, arr_C, arr_D, arr_E, arr_F, arr_G, arr_H, arr_I, arr_J, arr_K, arr_L, arr_M, arr_N, arr_O, arr_P, arr_Q, arr_R, arr_S, arr_T, arr_U, arr_V, arr_W, arr_X, arr_Y, arr_Z
+};
